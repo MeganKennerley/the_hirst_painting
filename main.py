0331d345a393e95ead6c0e449cf06af4df78bf5b
Megan <111366835+MeganKennerley@users.noreply.github.com>
2023-06-21 14:32:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# import colorgram
+#
+# colors = colorgram.extract('image.jpg', 10)
+#
+# color_tuple = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     color_tuple.append(new_color)
+import random
+import turtle
+from turtle import Turtle, Screen
+
+tim = Turtle()
+turtle.colormode(255)
+tim.penup()
+tim.speed("fastest")
+tim.hideturtle()
+
+color_list = [(56, 108, 149), (226, 132, 55), (197, 145, 172), (234, 201, 101), (145, 81, 54), (140, 180, 207), (233, 225, 195), (138, 130, 73)]
+
+def draw_dots():
+    i = 0
+    x = -250
+    y = -250
+    while i < 10:
+        tim.setx(x)
+        tim.sety(y)
+        for _ in range(0, 10):
+            tim.dot(20, random.choice(color_list))
+            tim.forward(50)
+        i += 1
+        y += 50
+
+
+draw_dots()
+
+
+screen = Screen()
+screen.exitonclick()
